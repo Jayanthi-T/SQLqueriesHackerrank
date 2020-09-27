e8d8ba635b3c2b53642554d06f495400b2a5cf9f
Jayanthi-T <66419308+Jayanthi-T@users.noreply.github.com>
2020-09-27 17:29:03 +0530

    Update SQLQueries.sql

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -13,7 +13,7 @@ select * from city where ID=1661;
 
     Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN
 select * from city where countrycode='JPN';
-
+  
      Query a list of CITY and STATE from the STATION table.   
 select city,state from station;
 
